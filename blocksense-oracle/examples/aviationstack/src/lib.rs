219911d6045d551c2107c016ee858d0da0acdab5
taylorferran <tailzpk@hotmail.co.uk>
2024-10-19 00:13:30 +0300

    Add hardhat stuffs

--- blocksense-oracle/examples/aviationstack/src/lib.rs
+++ blocksense-oracle/examples/aviationstack/src/lib.rs
@@ -59,10 +59,10 @@ struct FlightInfo {
 
 #[derive(Debug, Serialize, Deserialize)]
 struct AircraftInfo {
-    registration: String,
-    iata: String,
-    icao: String,
-    icao24: String,
+    registration: Option<String>,
+    iata: Option<String>,
+    icao: Option<String>,
+    icao24: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -99,8 +99,6 @@ struct FlightResponse {
 #[oracle_component]
 async fn oracle_request(settings: Settings) -> Result<Payload> {
 
-    println!("1XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-
     let data_feed = settings.data_feeds.first().unwrap();
     let url = match data_feed.data.as_str() {
         "FR1730" => Url::parse("https://api.aviationstack.com/v1/flights?access_key=f323a3a96ca09fd72db3029c3ad3fbb0&flight_iata=FR1730")?,
@@ -119,20 +117,23 @@ async fn oracle_request(settings: Settings) -> Result<Payload> {
     let body = resp.into_body();
     let string = String::from_utf8(body).expect("Our bytes should be valid utf8");
     let flight_response: FlightResponse = serde_json::from_str(&string).unwrap();
-    //let value: Rate = serde_json::from_str(&string).unwrap();
 
+    let flight_status = flight_response.data[0].flight_status.clone();
+    let status_code = match flight_status.as_str() {
+        "active" => 0,
+        "landed" => 1,
+        "cancelled" => 2,
+        _ => -1,
+    };
+    let status_code_f64: f64 = status_code as f64;
 
-    println!("2XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-    println!("{:?}", flight_response);
-    println!("3XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-    println!("{}", flight_response.data[0].flight_status.clone());
-    println!("4XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+    println!("Status code: {}", status_code);
 
     let mut payload: Payload = Payload::new();
     
     payload.values.push(DataFeedResult {
         id: data_feed.id.clone(),
-        value: DataFeedResultValue::Text(flight_response.data[0].flight_status.clone()),
+        value: DataFeedResultValue::Numerical(status_code_f64),
     });
     Ok(payload)
 }
