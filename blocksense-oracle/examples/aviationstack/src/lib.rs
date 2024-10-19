2314c263f7267d9c5e86427601a868cd1c3534d4
taylorferran <tailzpk@hotmail.co.uk>
2024-10-19 05:04:31 +0300

    Cleanup/fix

--- blocksense-oracle/examples/aviationstack/src/lib.rs
+++ blocksense-oracle/examples/aviationstack/src/lib.rs
@@ -27,66 +27,9 @@ struct Pagination {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct AirportInfo {
-    airport: String,
-    timezone: String,
-    iata: String,
-    icao: String,
-    terminal: Option<String>,
-    gate: Option<String>,
-    delay: Option<u32>,
-    scheduled: String,
-    estimated: String,
-    actual: Option<String>,
-    estimated_runway: Option<String>,
-    actual_runway: Option<String>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct AirlineInfo {
-    name: String,
-    iata: String,
-    icao: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct FlightInfo {
-    number: String,
-    iata: String,
-    icao: String,
-    codeshared: Option<String>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct AircraftInfo {
-    registration: Option<String>,
-    iata: Option<String>,
-    icao: Option<String>,
-    icao24: Option<String>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct LiveFlightData {
-    updated: String,
-    latitude: f64,
-    longitude: f64,
-    altitude: f64,
-    direction: f64,
-    speed_horizontal: f64,
-    speed_vertical: f64,
-    is_ground: bool,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
 struct Flight {
     flight_date: String,
     flight_status: String,
-    departure: AirportInfo,
-    arrival: AirportInfo,
-    airline: AirlineInfo,
-    flight: FlightInfo,
-    aircraft: AircraftInfo,
-    live: Option<LiveFlightData>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -118,7 +61,7 @@ async fn oracle_request(settings: Settings) -> Result<Payload> {
     let string = String::from_utf8(body).expect("Our bytes should be valid utf8");
     let flight_response: FlightResponse = serde_json::from_str(&string).unwrap();
 
-    let flight_status = flight_response.data[0].flight_status.clone();
+    let flight_status = flight_response.data[1].flight_status.clone();
     let status_code = match flight_status.as_str() {
         "active" => 0,
         "landed" => 1,
